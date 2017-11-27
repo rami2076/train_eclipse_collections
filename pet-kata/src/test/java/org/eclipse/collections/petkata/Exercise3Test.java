ff41173313fbbee03441478661d2ef3842b142b0
Tadashi Hashimoto <sakusennx@gmail.com>
2017-11-27 14:48:54 +0900

    Exercise3-1

--- pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise3Test.java
+++ pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise3Test.java
@@ -18,6 +18,7 @@ import org.eclipse.collections.api.map.MutableMap;
 import org.eclipse.collections.api.multimap.Multimap;
 import org.eclipse.collections.api.multimap.set.MutableSetMultimap;
 import org.eclipse.collections.api.set.MutableSet;
+import org.eclipse.collections.impl.factory.Bags;
 import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.factory.Maps;
 import org.eclipse.collections.impl.factory.Multimaps;
@@ -40,104 +41,138 @@ import org.junit.Test;
  *
  * @see <a href="http://eclipse.github.io/eclipse-collections-kata/pet-kata/#/6">Exercise 3 Slides</a>
  */
-public class Exercise3Test extends PetDomainForKata
-{
-    @Test
-    public void getCountsByPetType()
-    {
-        MutableList<PetType> petTypes = this.people.flatCollect(Person::getPets).collect(Pet::getType);
+public class Exercise3Test extends PetDomainForKata {
 
-        // Do you recognize this pattern?
-        MutableMap<PetType, Integer> petTypeCounts = Maps.mutable.empty();
-        for (PetType petType : petTypes)
-        {
-            Integer count = petTypeCounts.get(petType);
-            if (count == null)
-            {
-                count = 0;
-            }
-            petTypeCounts.put(petType, count + 1);
-        }
+	/*
+	 * getCountsByPetTypeでの課題で使用するメソッドは複数ある。
+	 * １.toBagでflatCollectionをbagにする。
+	 * 2.eachを使用し、以前に宣言したbagに追加する。
+	 * 3.mutableCollectionのcountByでBagにする。
+	 *
+	 * 1,2は8.2.0で使用できる。
+	 * 3.は９.0.0以降で使用できる。
+	 *
+	 * 今回このプロジェクトはeclipseCollectionsのgithubからcloneしてgradleプロジェクトとしてビルドした。
+	 * build.gradleで定義されているのは、8.2.0である。
+	 * mavenは9.0.0。
+	 * gradleの設定を変えることで、@seeで提示されている、solveと同じ回答で記述することができる。
+	 *
+	 *
+	 * そもそもBagとは。
+	 * 買い物をするときのレシートに商品Aを何点購入するのような概念。
+	 * もう少し抽象的に表現すると、itemの出現回数とitemをmap形式で重複なしで格納するための形式。
+	 * itemがkeyとなる。
+	 *
+	 * 様々な表現ができるが、どれが適切化までは判断できていない。
+	 */
 
-        Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.CAT));
-        Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.DOG));
-        Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.HAMSTER));
-        Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.SNAKE));
-        Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.TURTLE));
-        Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.BIRD));
+	@Test
+	public void getCountsByPetType() {
+		// 必要な宣言－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 
-        // Hint: use the appropriate method on this.people to create a Bag<PetType>
-        Bag<PetType> counts = null;
-        Assert.assertEquals(2, counts.occurrencesOf(PetType.CAT));
-        Assert.assertEquals(2, counts.occurrencesOf(PetType.DOG));
-        Assert.assertEquals(2, counts.occurrencesOf(PetType.HAMSTER));
-        Assert.assertEquals(1, counts.occurrencesOf(PetType.SNAKE));
-        Assert.assertEquals(1, counts.occurrencesOf(PetType.TURTLE));
-        Assert.assertEquals(1, counts.occurrencesOf(PetType.BIRD));
-    }
+		MutableList<PetType> petTypes = this.people.flatCollect(Person::getPets).collect(Pet::getType);
 
-    @Test
-    public void getPeopleByLastName()
-    {
-        // Do you recognize this pattern?
-        MutableMap<String, MutableList<Person>> lastNamesToPeople = Maps.mutable.empty();
-        for (Person person : this.people)
-        {
-            String lastName = person.getLastName();
-            MutableList<Person> peopleWithLastName = lastNamesToPeople.get(lastName);
-            if (peopleWithLastName == null)
-            {
-                peopleWithLastName = Lists.mutable.empty();
-                lastNamesToPeople.put(lastName, peopleWithLastName);
-            }
-            peopleWithLastName.add(person);
-        }
-        Verify.assertIterableSize(3, lastNamesToPeople.get("Smith"));
+		// EC以前の記述－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 
-        // Hint: use the appropriate method on this.people to create a Multimap<String, Person>
-        Multimap<String, Person> byLastNameMultimap = null;
+		// Do you recognize this pattern?
+		MutableMap<PetType, Integer> petTypeCounts = Maps.mutable.empty();
+		for (PetType petType : petTypes) {
+			Integer count = petTypeCounts.get(petType);
+			if (count == null) {
+				count = 0;
+			}
+			petTypeCounts.put(petType, count + 1);
+		}
 
-        Verify.assertIterableSize(3, byLastNameMultimap.get("Smith"));
-    }
+		Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.CAT));
+		Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.DOG));
+		Assert.assertEquals(Integer.valueOf(2), petTypeCounts.get(PetType.HAMSTER));
+		Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.SNAKE));
+		Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.TURTLE));
+		Assert.assertEquals(Integer.valueOf(1), petTypeCounts.get(PetType.BIRD));
 
-    @Test
-    public void getPeopleByTheirPets()
-    {
-        // Do you recognize this pattern?
-        MutableMap<PetType, MutableSet<Person>> peopleByPetType = Maps.mutable.empty();
+		// Bagを用いた記述－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 
-        for (Person person : this.people)
-        {
-            MutableList<Pet> pets = person.getPets();
-            for (Pet pet : pets)
-            {
-                PetType petType = pet.getType();
-                MutableSet<Person> peopleWithPetType = peopleByPetType.get(petType);
-                if (peopleWithPetType == null)
-                {
-                    peopleWithPetType = Sets.mutable.empty();
-                    peopleByPetType.put(petType, peopleWithPetType);
-                }
-                peopleWithPetType.add(person);
-            }
-        }
+		// Hint: use the appropriate method on this.people to create a Bag<PetType>
+		// 下記の書き方の場合finalが必要になる。下記の書き方の場合Bag<PetType>で記述できない。
+		// MutableBagはCollectionを継承。子 MutableBagIterable<T>→MutableCollection<T>→Collections<T> 親
+		final MutableBag<PetType> counts = Bags.mutable.empty();
+		petTypes.each(petType -> counts.add(petType));// countsは上の方でflattenされています。
 
-        Verify.assertIterableSize(2, peopleByPetType.get(PetType.CAT));
-        Verify.assertIterableSize(2, peopleByPetType.get(PetType.DOG));
-        Verify.assertIterableSize(1, peopleByPetType.get(PetType.HAMSTER));
-        Verify.assertIterableSize(1, peopleByPetType.get(PetType.TURTLE));
-        Verify.assertIterableSize(1, peopleByPetType.get(PetType.BIRD));
-        Verify.assertIterableSize(1, peopleByPetType.get(PetType.SNAKE));
+		// 以降の書き方の場合finalは不要。また、以下の書き方の場合Bag<PetType>でも可
+		MutableBag<PetType> counts_m = this.people.flatCollect(Person::getPets).collect(Pet::getType).toBag();
+		;
+		// EclipseCollections_8.2非対応 9.0.0から以下の書き方に対応。
+		MutableBag<PetType> counts_n = this.people.flatCollect(Person::getPets).countBy(Pet::getType);
 
-        // Hint: use the appropriate method on this.people with a target collection to create a MutableSetMultimap<String, Person>
-        // Hint: this.people is a MutableList, so it will return a MutableListMultimap without a target collection
-        MutableSetMultimap<PetType, Person> multimap = null;
+		Bag<PetType> counts_o = this.people.flatCollect(Person::getPets).collect(Pet::getType).toBag();
+		counts_o = this.people.flatCollect(Person::getPets).countBy(Pet::getType);
 
-        Verify.assertIterableSize(2, multimap.get(PetType.CAT));
-        Verify.assertIterableSize(2, multimap.get(PetType.DOG));
-        Verify.assertIterableSize(1, multimap.get(PetType.HAMSTER));
-        Verify.assertIterableSize(1, multimap.get(PetType.TURTLE));
-        Verify.assertIterableSize(1, multimap.get(PetType.BIRD));
-        Verify.assertIterableSize(1, multimap.get(PetType.SNAKE));
-    }
+		Assert.assertEquals(2, counts.occurrencesOf(PetType.CAT));
+		Assert.assertEquals(2, counts.occurrencesOf(PetType.DOG));
+		Assert.assertEquals(2, counts.occurrencesOf(PetType.HAMSTER));
+		Assert.assertEquals(1, counts.occurrencesOf(PetType.SNAKE));
+		Assert.assertEquals(1, counts.occurrencesOf(PetType.TURTLE));
+		Assert.assertEquals(1, counts.occurrencesOf(PetType.BIRD));
+
+		// －－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+	}
+
+	@Test
+	public void getPeopleByLastName() {
+		// Do you recognize this pattern?
+		MutableMap<String, MutableList<Person>> lastNamesToPeople = Maps.mutable.empty();
+		for (Person person : this.people) {
+			String lastName = person.getLastName();
+			MutableList<Person> peopleWithLastName = lastNamesToPeople.get(lastName);
+			if (peopleWithLastName == null) {
+				peopleWithLastName = Lists.mutable.empty();
+				lastNamesToPeople.put(lastName, peopleWithLastName);
+			}
+			peopleWithLastName.add(person);
+		}
+		Verify.assertIterableSize(3, lastNamesToPeople.get("Smith"));
+
+		// Hint: use the appropriate method on this.people to create a Multimap<String, Person>
+		Multimap<String, Person> byLastNameMultimap = null;
+
+		Verify.assertIterableSize(3, byLastNameMultimap.get("Smith"));
+	}
+
+	@Test
+	public void getPeopleByTheirPets() {
+		// Do you recognize this pattern?
+		MutableMap<PetType, MutableSet<Person>> peopleByPetType = Maps.mutable.empty();
+
+		for (Person person : this.people) {
+			MutableList<Pet> pets = person.getPets();
+			for (Pet pet : pets) {
+				PetType petType = pet.getType();
+				MutableSet<Person> peopleWithPetType = peopleByPetType.get(petType);
+				if (peopleWithPetType == null) {
+					peopleWithPetType = Sets.mutable.empty();
+					peopleByPetType.put(petType, peopleWithPetType);
+				}
+				peopleWithPetType.add(person);
+			}
+		}
+
+		Verify.assertIterableSize(2, peopleByPetType.get(PetType.CAT));
+		Verify.assertIterableSize(2, peopleByPetType.get(PetType.DOG));
+		Verify.assertIterableSize(1, peopleByPetType.get(PetType.HAMSTER));
+		Verify.assertIterableSize(1, peopleByPetType.get(PetType.TURTLE));
+		Verify.assertIterableSize(1, peopleByPetType.get(PetType.BIRD));
+		Verify.assertIterableSize(1, peopleByPetType.get(PetType.SNAKE));
+
+		// Hint: use the appropriate method on this.people with a target collection to create a MutableSetMultimap<String, Person>
+		// Hint: this.people is a MutableList, so it will return a MutableListMultimap without a target collection
+		MutableSetMultimap<PetType, Person> multimap = null;
+
+		Verify.assertIterableSize(2, multimap.get(PetType.CAT));
+		Verify.assertIterableSize(2, multimap.get(PetType.DOG));
+		Verify.assertIterableSize(1, multimap.get(PetType.HAMSTER));
+		Verify.assertIterableSize(1, multimap.get(PetType.TURTLE));
+		Verify.assertIterableSize(1, multimap.get(PetType.BIRD));
+		Verify.assertIterableSize(1, multimap.get(PetType.SNAKE));
+	}
 }
